c28d814e130f70035ecda8c97a644d9bad33d312
alemedinabjj <101877712+alemedinabjj@users.noreply.github.com>
2022-04-21 20:14:08 -0300

    update

--- ver.js
+++ ver.js
@@ -2,6 +2,37 @@ const getBanco = () => JSON.parse(localStorage.getItem("todoList")) ?? [];
 const setBanco = (banco) =>
     localStorage.setItem("todoList", JSON.stringify(banco));
 
+    
+    function getUserPosition() {
+        let url;
+        navigator.geolocation.getCurrentPosition((pos) => {
+          let lat = pos.coords.latitude;
+          let long = pos.coords.longitude;
+          url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&APPID=95b11822eb429c84c1143a19251b1881`;
+          fetchApi(url);
+        });
+      }
+    
+      function fetchApi(url) {
+        let city = document.querySelector('.city');
+        let temp = document.querySelector('span');
+        fetch(url)
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          let tempInCelsius = ((5/9) * (data.main.temp-32)).toFixed(1);
+          city.innerText = `${data.name}`;
+          temp.innerText = tempInCelsius;
+        })
+        .catch((err) => {
+          city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
+          temp.innerText = `-`;
+        })
+      }
+      
+      getUserPosition();
+      
 const atualizarRel = () => {
     let relogio = document.querySelector('.relogio')
     let diaSem = document.querySelector('.diasem')
